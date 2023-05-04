8228d7892748b02cc2232c7621d5d86699e2ddbc
Keila Vilchez <3006560840101@ingenieria.usac.edu.gt>
2023-05-03 18:44:30 -0600

    insert Cassandra

--- Proyecto2/backend/src/controllers/cassanda.js
+++ Proyecto2/backend/src/controllers/cassanda.js
@@ -34,12 +34,53 @@ const consulta1 = async (req, res) => {
 
 const consulta2 = async (req, res) => {
     try {
-        let pacientes = [
-            { Categoria: "PEDIATRICO", Total_paciente: await (await executeC(`select count(*) from Paciente where edad < 18 ALLOW FILTERING`)).rows[0]['count'].low },
-            { Categoria: "MEDIANA EDAD", Total_paciente: await (await executeC(`select count(*) from Paciente where edad >= 18 ALLOW FILTERING`)).rows[0]['count'].low },
-            { Categoria: "GERIATRICO", Total_paciente: await (await executeC(`select count(*) from Paciente where edad > 64 ALLOW FILTERING`)).rows[0]['count'].low }
-        ];     
-        let json = JSON.stringify(pacientes);
+        // Obtener los datos de Cassandra
+        let consulta = (await executeC(`SELECT * FROM logActividad`)).rows;
+
+        // Objeto para almacenar los conteos por habitación
+        let conteos = {};
+
+        // Iterar por las filas de la consulta
+        for (let fila of consulta) {
+        // Obtener el id de la habitación y el id del paciente
+            let idhabitacion = fila.idhabitacion;
+            let idpaciente = fila.idpaciente;
+
+            // Verificar si la habitación ya ha sido contada
+            if (idhabitacion in conteos) {
+                // Si la habitación ya ha sido contada, incrementar el conteo de pacientes
+                conteos[idhabitacion]++;
+            } else {
+                // Si la habitación no ha sido contada, iniciar el conteo con 1
+                conteos[idhabitacion] = 1;
+            }
+        }
+
+     
+        let r=[]
+        for(let idhabitacion in conteos){
+            r.push((await executeC(`SELECT * FROM habitacion where idhabitacion=`+ idhabitacion)).rows)
+        }
+
+        console.log(r)
+        let habitaciones = [
+            { habitacion:r[0][0].habitacion,count:conteos[1]},
+            { habitacion:r[1][0].habitacion,count:conteos[2]},
+            { habitacion:r[2][0].habitacion,count:conteos[3]},
+            { habitacion:r[3][0].habitacion,count:conteos[4]},
+            { habitacion:r[4][0].habitacion,count:conteos[5]},
+            { habitacion:r[5][0].habitacion,count:conteos[6]},
+            { habitacion:r[6][0].habitacion,count:conteos[7]},
+            { habitacion:r[7][0].habitacion,count:conteos[8]},
+            { habitacion:r[8][0].habitacion,count:conteos[9]},
+            { habitacion:r[9][0].habitacion,count:conteos[10]},
+            { habitacion:r[10][0].habitacion,count:conteos[11]},
+            { habitacion:r[11][0].habitacion,count:conteos[12]},
+            { habitacion:r[12][0].habitacion,count:conteos[13]},
+            { habitacion:r[13][0].habitacion,count:conteos[14]},
+            { habitacion:r[14][0].habitacion,count:conteos[15]},
+       ];       
+        let json = JSON.stringify(habitaciones);
 
         res.status(200).json({
             'data': json
@@ -144,6 +185,7 @@ const consulta5 = async (req, res) => {
 const consulta6 = async (req, res) => {
     try {
         let consulta = (await executeC(`SELECT * FROM logActividad`)).rows;
+        console.log(consulta);
        
         let cantidadArray = consulta.map(obj => obj.idhabitacion);
         
@@ -167,13 +209,13 @@ const consulta6 = async (req, res) => {
             r.push((await executeC(`SELECT * FROM habitacion where idhabitacion=`+frecuencia_ordenada[i][0])).rows)
         }
         
-        console.log(r);
+        
         let habitaciones = [
-            { idhabitacion: r[0][0],habitacion:r[0][1],count:frecuencia_ordenada[0][1]},
-            { idhabitacion: r[1][0],habitacion:r[1][1],count:frecuencia_ordenada[0][1]},
-            { idhabitacion: r[2][0],habitacion:r[2][1],count:frecuencia_ordenada[0][1]},
-            { idhabitacion: r[3][0],habitacion:r[3][1],count:frecuencia_ordenada[0][1]},
-            { idhabitacion: r[4][0],habitacion:r[4][1],count:frecuencia_ordenada[0][1]},
+            { habitacion:r[0][0].habitacion,count:frecuencia_ordenada[0][1]},
+            { habitacion:r[1][0].habitacion,count:frecuencia_ordenada[1][1]},
+            { habitacion:r[2][0].habitacion,count:frecuencia_ordenada[2][1]},
+            { habitacion:r[3][0].habitacion,count:frecuencia_ordenada[3][1]},
+            { habitacion:r[4][0].habitacion,count:frecuencia_ordenada[4][1]},
        ];       
         let json = JSON.stringify(habitaciones);
 
@@ -214,12 +256,12 @@ const consulta7 = async (req, res) => {
         
         console.log(r);
         let habitaciones = [
-             { idhabitacion: r[0][0],habitacion:r[0][1],count:frecuencia_ordenada[0][1]},
-             { idhabitacion: r[1][0],habitacion:r[1][1],count:frecuencia_ordenada[0][1]},
-             { idhabitacion: r[2][0],habitacion:r[2][1],count:frecuencia_ordenada[0][1]},
-             { idhabitacion: r[3][0],habitacion:r[3][1],count:frecuencia_ordenada[0][1]},
-             { idhabitacion: r[4][0],habitacion:r[4][1],count:frecuencia_ordenada[0][1]},
-        ];     
+            { habitacion:r[0][0].habitacion,count:frecuencia_ordenada[0][1]},
+            { habitacion:r[1][0].habitacion,count:frecuencia_ordenada[1][1]},
+            { habitacion:r[2][0].habitacion,count:frecuencia_ordenada[2][1]},
+            { habitacion:r[3][0].habitacion,count:frecuencia_ordenada[3][1]},
+            { habitacion:r[4][0].habitacion,count:frecuencia_ordenada[4][1]},
+       ];        
         let json = JSON.stringify(habitaciones);
 
         res.status(200).json({
@@ -230,5 +272,58 @@ const consulta7 = async (req, res) => {
     }
     
 }
+
+const insertLogHabitacion = async (req, res) => {
+    const info = req.body;
+
+    try {
+        const fecha = new Date();
+        const dia = fecha.getDate();
+        const mes = fecha.getMonth() + 1; // Los meses comienzan desde 0
+        const anio = fecha.getFullYear();
+        const hora = fecha.getHours();
+        const minutos = fecha.getMinutes();
+        const segundos = fecha.getSeconds();
+        const min = 34619;
+        const max = 100000; // Puedes ajustar este valor para obtener un número aleatorio mayor o menor.
+
+        const numAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+        const fechaActual = `${dia}/${mes}/${anio} ${hora}:${minutos}:${segundos}`;
+        let consulta = (await executeC(`INSERT INTO logHabitacion(timestampx, statusx, idhabitacion, id) values('${fechaActual}','${info.statusx}',${info.idhabitacion},${numAleatorio}) `)).rows;
+        res.status(200).json({
+            'data': consulta
+        }); 
+    } catch (error) {
+        res.status(200).json({ 'success': false, 'message': 'Existe un error inesperado',error })
+    }
+    
+}
+
+const insertLogActividad = async (req, res) => {
+    const info = req.body;
+
+    try {
+        const fecha = new Date();
+        const dia = fecha.getDate();
+        const mes = fecha.getMonth() + 1; // Los meses comienzan desde 0
+        const anio = fecha.getFullYear();
+        const hora = fecha.getHours();
+        const minutos = fecha.getMinutes();
+        const segundos = fecha.getSeconds();
+        const min = 300000;
+        const max = 500000; // Puedes ajustar este valor para obtener un número aleatorio mayor o menor.
+
+        const numAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+        const fechaActual = `${dia}/${mes}/${anio} ${hora}:${minutos}:${segundos}`;
+        let consulta = (await executeC(`INSERT INTO logActividad(timestampx, actividad,idpaciente, idhabitacion, id) values('${fechaActual}','${info.actividad}',${info.idpaciente},${info.idhabitacion},${numAleatorio}) `)).rows;
+        res.status(200).json({
+            'data': consulta
+        }); 
+    } catch (error) {
+        res.status(200).json({ 'success': false, 'message': 'Existe un error inesperado',error })
+    }
+    
+}
+
 //cassandraQuery()
-module.exports = { consulta1,consulta2,consulta3,consulta4, consulta5, consulta6, consulta7};
+module.exports = { consulta1,consulta2,consulta3,consulta4, consulta5, consulta6, consulta7,insertLogHabitacion,insertLogActividad};
